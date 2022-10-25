8afbce9844701df730249d05dee2a97749336286
devin <devinelliotgomez@gmail.com>
2022-10-24 20:53:45 -0500

    still having much problems with the javascript

--- game.js
+++ game.js
@@ -1,6 +1,24 @@
 const correct = document.querySelector ('#correct')
+const incorrect = document.querySelector ('#incorrect')
+const submission = document.querySelector ('#submission')
+
+
+let score = 0;
+let highscores = []
 
 correct.addEventListener('click', () => {
     correct.classList.remove('.button2')
     correct.classList.add('.correct')
+    score++
 })
+
+incorrect.addEventListener('click', () => {
+    correct.classList.remove('.button2')
+    correct.classList.add('.incorrect')
+    score--
+})
+
+submission.addEventListener("click", () => {
+    var submissions =
+})
+
