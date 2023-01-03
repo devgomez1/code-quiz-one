e892126e04107d1ef379737f3f6f255047a13728
devin <devinelliotgomez@gmail.com>
2023-01-02 19:23:36 -0600

    added js so that when you click a correct or incorrect answer it changes the color of the button indicating if it was correct or not

--- game.js
+++ game.js
@@ -1,9 +1,19 @@
 const correctEl = document.getElementById("correct");
 const incorrectEl = document.getElementById("incorrect");
+const incorrectEl2 = document.getElementById("incorrect2");
+const incorrectEl3 = document.getElementById("incorrect3");
 
-function changeClass() {
-    correctEl.classList.remove('button2');
-    correctEl.classList.add('correct');
+function correctClass() {
+  correctEl.classList.remove("button2");
+  correctEl.classList.add("correct");
 }
 
-correctEl.addEventListener('click', changeClass);
+function incorrectClass(event) {
+  event.target.classList.remove("button2");
+  event.target.classList.add("incorrect");
+}
+
+correctEl.addEventListener("click", correctClass);
+incorrectEl.addEventListener("click", incorrectClass);
+incorrectEl2.addEventListener("click", incorrectClass);
+incorrectEl3.addEventListener("click", incorrectClass);
