564931d32fbce106881ab6eb15cb97a39126f0c0
devin <devinelliotgomez@gmail.com>
2023-01-02 18:48:40 -0600

    working on changing classes when buttons are clicked

--- game.js
+++ game.js
@@ -1,26 +1,9 @@
-const correct = document.getElementById('correct');
-const incorrect = document.querySelector ('#incorrect')
-const submission = document.querySelector ('#submission')
+const correctEl = document.getElementById("correct");
+const incorrectEl = document.getElementById("incorrect");
 
+function changeClass() {
+    correctEl.classList.remove('button2');
+    correctEl.classList.add('correct');
+}
 
-let score = 0;
-let highscores = []
-
-correct.addEventListener('click', () => {
-    correct.classList.remove('.button2')
-    correct.classList.add('.correct')
-    score++
-})
-
-incorrect.addEventListener('click', () => {
-    correct.classList.remove('.button2')
-    correct.classList.add('.incorrect')
-    score--
-})
-
-submission.addEventListener("click", () => {
-    var submissions = submission.textContent
-    push.submissions(highscores)
-})
-
-
+correctEl.addEventListener('click', changeClass);
