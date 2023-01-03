5eb1d5e103e318b26e950cfbafba7b4693f82bbb
devin <devinelliotgomez@gmail.com>
2023-01-02 19:58:55 -0600

    playing around with saving the score to local storage

--- game.js
+++ game.js
@@ -2,18 +2,29 @@ const correctEl = document.getElementById("correct");
 const incorrectEl = document.getElementById("incorrect");
 const incorrectEl2 = document.getElementById("incorrect2");
 const incorrectEl3 = document.getElementById("incorrect3");
+const userScore = document.getElementById("scores");
+
+let score = sessionStorage.getItem('score') || 0;
 
 function correctClass() {
   correctEl.classList.remove("button2");
   correctEl.classList.add("correct");
+  score++;
+  sessionStorage.setItem('score', score);
+  console.log(score);
 }
 
 function incorrectClass(event) {
   event.target.classList.remove("button2");
   event.target.classList.add("incorrect");
+  score--;
+  sessionStorage.setItem('score', score);
+  console.log(score);
 }
 
 correctEl.addEventListener("click", correctClass);
 incorrectEl.addEventListener("click", incorrectClass);
 incorrectEl2.addEventListener("click", incorrectClass);
 incorrectEl3.addEventListener("click", incorrectClass);
+
+
